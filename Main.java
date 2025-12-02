a67cd3d54caa99196a8ab303830b7b1fe8d7150e
Toavina Rova <toavinarovamampionina@gmail.com>
2025-12-02 12:51:08 +0000

    ajout fichiers main et villestats

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        VilleStats villeStats = new VilleStats();
-
-        villeStats.ajouterVille("Paris", 2200000);
-        villeStats.ajouterVille("Tokyo", 13900000);
-        villeStats.ajouterVille("Antananarivo", 1500000);
-        villeStats.ajouterVille("New York", 8500000);
-
-        villeStats.afficherVille();
+        VilleStats liste = new VilleStats();
+        liste.ajouterVille("Paris", 2200000);
+        liste.ajouterVille("Tokyo", 13900000);
+        liste.ajouterVille("Antananarivo", 1500000);
+        liste.ajouterVille("New York", 8500000);
+ 
+        liste.moyennePopulation();
     }
 }
