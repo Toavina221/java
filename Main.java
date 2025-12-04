66c1cd401ebba12ebd84e0b19dd79568fd13f131
Toavina Rova <toavinarovamampionina@gmail.com>
2025-12-04 12:44:23 +0000

    Modifivcation de NotesScolaires en GestionNotes

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
  public class Main {
     public static void main(String[] args) {
-        NotesScolaires notesScolaires = new NotesScolaires();
-        notesScolaires.ajouterEleve("Rova");
+        
     }
  }
