edd1d0140ece0182b0e93b9daa02586cee5decc4
Toavina Rova <toavinarovamampionina@gmail.com>
2025-12-05 12:51:22 +0000

    ajout fichier

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
  public class Main {
     public static void main(String[] args) {
-        
+        GestionNotes notesEleves = new GestionNotes();
+        notesEleves.ajouterEleve("Toavina");
+        notesEleves.ajouterNote("Toavina", 12); 
+        notesEleves.ajouterNote("Toavina", 17); 
+        notesEleves.calculerMoyenne("Toavina");
     }
  }
